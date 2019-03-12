1b15772babae551bdf86a52ca751fa4f25eaae3c
JessicaRoberts <lexington123>
2019-03-12 11:34:21 -0400

    right branch url connection lab

--- app/src/main/java/com/introtoandroid/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/introtoandroid/csci490_lab_04/MainActivity.java
@@ -3,11 +3,88 @@ package com.introtoandroid.csci490_lab_04;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected String myMethod (String isString) throws IOException {
+        URL url = null;
+
+        try {
+
+            url = new URL(isString);
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        }
+
+        URLConnection connection = null;
+
+        try {
+
+            connection = url.openConnection();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+        InputStreamReader inputStreamReader = null;
+
+        try {
+
+            inputStreamReader = new InputStreamReader((connection.getInputStream()));
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String line = null;
+
+        try {
+
+            line = bufferedReader.readLine();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+        int i = 0;
+
+        while(i < 20){
+
+            try {
+
+                line += bufferedReader.readLine();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+            i++;
+
+        }
+
+        return isString;
     }
 }
